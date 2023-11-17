10fbde8818dd21f70b7f1e71f6371a7c601e97bc
crosscap <wangliankun2018@outlook.com>
2023-11-17 15:57:18 +0800

    change some codes in essential

--- essential/ch06/BinaryTree.cc
+++ essential/ch06/BinaryTree.cc
@@ -1,15 +1,6 @@
 #include "BinaryTree.h"
 
 template <typename valType>
-inline BTnode<valType>::
-BTnode(const valType &val)
-    : _val(val)
-{
-    _cnt = 1;
-    _lchild = _rchild = nullptr;
-}
-
-template <typename valType>
 void BTnode<valType>::
 insert_value(const valType &val)
 {
@@ -67,61 +58,6 @@ lchild_leaf(BTnode *leaf, BTnode *subtree)
     while (subtree->_lchild)
         subtree = subtree->_lchild;
     subtree->_lchild = leaf;
-}
-
-template <typename elemType>
-inline BinaryTree<elemType>::
-BinaryTree()
-    : _root(nullptr)
-{ }
-
-template <typename elemType>
-inline BinaryTree<elemType>::
-BinaryTree(const BinaryTree &rhs)
-{
-    copy(_root, rhs._root);
-}
-
-template <typename elemType>
-inline BinaryTree<elemType>::
-~BinaryTree()
-{
-    clear();
-}
-
-template <typename elemType>
-inline void BinaryTree<elemType>::
-insert(const elemType &elem)
-{
-    if (!_root)
-        _root = new BTnode<elemType>(elem);
-    else
-        _root->insert_value(elem);
-}
-
-template <typename elemType>
-inline BinaryTree<elemType> &
-BinaryTree<elemType>::
-operator=(const BinaryTree &rhs)
-{
-    if (this != &rhs) {
-        clear();
-        copy(_root, rhs._root);
-    }
-    return *this;
-}
-
-template<typename elemType>
-inline void
-BinaryTree<elemType>::
-remove(const elemType &elem)
-{
-    if (_root) {
-        if (_root->_val == elem)
-            remove_root();
-        else
-            _root->remove_value(elem, _root);
-    }
 }
 
 template<typename elemType>
@@ -226,12 +162,4 @@ print( ostream &os,
 {
 	(this->*traversal)( os );
 	return os;
-}
-
-template <typename elemType>
-inline ostream& operator<<(ostream &os, const BinaryTree<elemType> &bt)
-{
-    os << "Tree: " << endl;
-    bt.print(os);
-    return os;
 }
