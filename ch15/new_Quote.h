f66dc0e108bf788d35c00b6e6d28652dac00dcb4
crosscap <wangliankun2018@outlook.com>
2023-12-21 21:34:40 +0800

    finish ch15-07 to ch15-08

--- ch15/new_Quote.h
+++ ch15/new_Quote.h
@@ -12,11 +12,18 @@ public:
 	Quote() = default;
 	Quote(const std::string &book, double sales_price)
 		: bookNo(book), price(sales_price) { }
+	Quote(const Quote &) = default;
+	Quote(Quote &&) = default;
+	Quote &operator=(const Quote &) = default;
+	Quote &operator=(Quote &&) = default;
 	virtual ~Quote() = default;
 
 	std::string isbn() const { return bookNo; }
 	virtual double net_price(std::size_t n) const { return n * price; }
 	virtual ostream &debug(ostream & = cout) const;
+
+	virtual Quote *clone() const & { return new Quote(*this); }
+	virtual Quote *clone() && { return new Quote(std::move(*this)); }
 
 private:
 	std::string bookNo;
@@ -45,7 +52,11 @@ public:
 	Bulk_quote() = default;
 	Bulk_quote(const std::string &book, double price, size_t qty, double disc)
 		: Disc_quote(book, price, qty, disc) { }
+
 	double net_price(std::size_t n) const override;
+
+	Bulk_quote *clone() const & override { return new Bulk_quote(*this); }
+	Bulk_quote *clone() && override { return new Bulk_quote(std::move(*this)); }
 };
 
 class Limit_quote : public Disc_quote
@@ -57,9 +68,10 @@ public:
 	double net_price(std::size_t n) const override;
 };
 
-inline ostream &Quote::debug(ostream & os) const
+inline ostream &Quote::debug(ostream &os) const
 {
 	os << "bookNo: " << bookNo << " price: " << price;
+	return os;
 }
 
 inline double Bulk_quote::net_price(std::size_t cnt) const
