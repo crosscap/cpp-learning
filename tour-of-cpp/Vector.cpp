afa45ff9f9491f39f7402e3b8d93703b5322683d
crosscap <wangliankun2018@outlook.com>
2025-02-21 20:56:28 +0800

    Add error handling in Vector

--- tour-of-cpp/Vector.cpp
+++ tour-of-cpp/Vector.cpp
@@ -1,14 +1,24 @@
 #include "Vector.h"
+#include <stdexcept>
 
 Vector::Vector(int s)
-    : elem{new double[s]}, sz{s} { }
+{
+	if (s < 0) {
+		throw std::length_error{"Vector::Vector"};
+	}
+	elem = new double[s];
+	sz = s;
+}
 
 double& Vector::operator[](int i)
 {
-    return elem[i];
+	if (i < 0 || i >= sz) {
+		throw std::out_of_range{"Vector::operator[]"};
+	}
+	return elem[i];
 }
 
 int Vector::size()
 {
-    return sz;
+	return sz;
 }
